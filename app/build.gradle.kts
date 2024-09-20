4c62d0429e6cce5626247f7a985258d2842210dd
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-20 20:25:45 +0300

    updated: scroll view

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.ismailmesutmujde.kotlinscrollview"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.ismailmesutmujde.kotlinscrollview"
         minSdk = 24
